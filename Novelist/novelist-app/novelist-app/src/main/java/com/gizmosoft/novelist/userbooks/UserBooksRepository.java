c92833e28040323aff014f14591017f3ce36773d
Gizmosoft <Kartikey.hebbar@gmail.com>
2022-06-22 23:05:56 +0530

    change done

--- Novelist/novelist-app/novelist-app/src/main/java/com/gizmosoft/novelist/userbooks/UserBooksRepository.java
+++
@@ -1,6 +0,0 @@
-package com.gizmosoft.novelist.userbooks;
-import org.springframework.data.cassandra.repository.CassandraRepository;
-
-public interface UserBooksRepository extends CassandraRepository<UserBooks, UserBooksPrimarykey>{
-    
-}
